cb3e0845afab97eea90b14fbf37547509836a09d
John Slowik <john@jslowik.com>
2020-04-05 19:00:49 -0500

    Bumping more

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     include_package_data=True,
     install_requires=[
         'emoji==0.4.5',
-        'h5py==2.7.0',
-        'Keras==2.0.9',
+        'h5py==2.10.0',
+        'Keras==2.3.1',
         'numpy==1.18.2',
         'scikit-learn==0.19.0',
         'tensorflow==1.13.2',
