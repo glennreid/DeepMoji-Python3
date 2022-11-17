7e2f95d20adcbaa69cbcfd05b8cf1867c3677d9b
Glenn Reid <git@glennreid.com>
2022-11-17 11:28:30 -0600

    numpy==1.19.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'emoji==0.6.0',
         'h5py==2.10.0',
         'Keras==2.3.1',
-        'numpy==1.18.2',
+        'numpy==1.19.2',
         'scikit-learn==0.22.2.post1',
         'tensorflow==2.4.1',
         'text-unidecode==1.0',
